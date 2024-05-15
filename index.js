14c711f661c8bf37eab6bc980c9dce5c1f995836
Matthew Polk <matthewpolkofficial@gmail.com>
2024-05-14 20:32:30 -0500

    Javascript start of implementation

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function playButton(){
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    count = 0;
+    count++;
+    if(count % 2 === 0)
+    {
+        document.getElementById("demo").innerHTML = "ButtonON";
+    }
+    if(count % 1 === 0)
+    {
+        document.getElementById("demo").innerHTML = "ButtonOFF";
+    }
+    console.log(count);
 }
   
