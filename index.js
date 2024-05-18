4f6b92daa04472e9bb188ce9c33b88c2d3118ec3
Matthew Polk <matthewpolkofficial@gmail.com>
2024-05-18 02:27:49 -0500

    Added button to return back to main page.

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function playButton(){
-    count = 0;
-    count++;
-    if(count % 2 === 0)
-    {
-        document.getElementById("demo").innerHTML = "ButtonON";
-    }
-    if(count % 1 === 0)
-    {
-        document.getElementById("demo").innerHTML = "ButtonOFF";
-    }
-    console.log(count);
+function returnButton(){
+    history.back();
 }
   
